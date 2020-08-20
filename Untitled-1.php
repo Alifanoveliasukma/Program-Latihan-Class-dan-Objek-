cde5961f07619eb6d425dabfbef1ad8ddf945f5e
Alifanoveliasukma <69952596+Alifanoveliasukma@users.noreply.github.com>
2020-08-20 15:27:21 +0700

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,13 @@
+<?php
+class latihan {
+   //... isi dari class latihan
+   }
+ 
+$a = new latihan();
+echo var_dump($a)."<br>";
+$b = new latihan ();
+echo var_dump($b)."<br>";
+$c = new coba();
+echo var_dump($c)."<br>";
+
+?>
